512b573875e52fe0aa5ac08d8e1205ade59647cb
Azamat Khafizov <80072898+Aze1ool@users.noreply.github.com>
2022-04-14 15:08:53 +0300

    Update skript.py

--- skript.py
+++ skript.py
@@ -1,7 +1,9 @@
 #Azamat Khafizov
 #Python Project
 #Receipt for Lovely Loveseats
-
+"""
+In this project, we will be storing the names and prices of a furniture store’s catalog in variables. We will then process the total price and item list of customers, printing them to the output terminal.
+"""
 lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
 lovely_loveseat_price = 254.00
 stylish_settee_decription = "Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
